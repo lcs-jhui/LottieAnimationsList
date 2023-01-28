0d935a422d568bbd67f14df67481f3d4c02cad2b
Justin Hui <jhui@lcs.on.ca>
2023-01-28 10:05:53 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by Justin Hui on 28/1/2023.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsListView()
+    }
+}
